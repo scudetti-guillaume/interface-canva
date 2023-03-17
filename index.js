821af48472f8dd9b86eb71b49a960141953381ce
scudetti-guillaume <124565246+scudetti-guillaume@users.noreply.github.com>
2023-03-17 21:41:32 +0100

    ADD check double click => selected rectangle

--- index.js
+++ index.js
@@ -5,20 +5,30 @@ const context = canvas.getContext('2d');
 const rectangles = [];
 const excludedColors = []
 let isDragging = false;
+let doubleClickCheck = false;
 
+// redimensionne le canva en fonction de la taille de la fenêtre
 const resizeCanvas = () => {
   canvas.width = canvas.parentElement.clientWidth;
   canvas.height = canvas.parentElement.clientHeight;
   redrawRectangles();
 }
 
+// redessine les rectangles 
 const redrawRectangles = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  rectangles.forEach((rectangle) => rectangle.draw(context));
-}
+  rectangles.forEach((rectangle, index) => {
+  // supprime du tableau ceux hors canvas
+    if (rectangle.x < 0 || rectangle.y < 0 || rectangle.x + rectangle.width > canvas.width || rectangle.y + rectangle.height > canvas.height) {
+      rectangles.splice(index, 1);
+    } else {
+      rectangle.draw(context);
+    }
+  });
+};
 
 
-// donne une couleur aléatoire au rectangle 
+// donne une couleur aléatoire au rectangle en excluant celle deja existante ou celle du backgroung
 const getRandomColor = () => {
   const letters = "0123456789ABCDEF";
   const backgroundColor = getComputedStyle(canvas).backgroundColor;
@@ -31,22 +41,24 @@ const getRandomColor = () => {
   if (excludedColors.includes(color)) {
     return getRandomColor();
   } else {
-   excludedColors.push(color);
+    excludedColors.push(color);
     return color;
   }
 }
 
+// demarre le dessin du rectangle
 const startDragging = (event) => {
-
-  const color = getRandomColor();
-  const startX = event.offsetX;
-  const startY = event.offsetY;
-  const rectangle = new Rectangle(startX, startY, 0, 0, color);
-  rectangles.push(rectangle);
-  console.log(rectangles);
-  isDragging = true;
+event.preventDefault();
+    const color = getRandomColor();
+    const startX = event.offsetX;
+    const startY = event.offsetY;
+    const rectangle = new Rectangle(startX, startY, 0, 0, color);
+    rectangles.push(rectangle);
+    isDragging = true;
+    console.log(rectangles);
 }
 
+// assure le dessin du rectangle
 const drag = (event) => {
   if (isDragging) {
     const rectangle = rectangles[rectangles.length - 1];
@@ -55,14 +67,27 @@ const drag = (event) => {
     redrawRectangles();
   }
 }
-
+// stop le dessin du rectangle
 const stopDragging = () => {
   isDragging = false;
 }
+
+// check un double click
+const handleDoubleClick = (event) => {
+  event.preventDefault();
+  doubleClickCheck = true
+  // supprime du tableau les deux derniers rectangles creer dû au double click et redessine le tableau
+  if (doubleClickCheck && rectangles.length >= 1) {
+    rectangles.splice(rectangles.length - 2, 2);
+    redrawRectangles();
+  }
+}
+
 
 canvas.addEventListener('mousedown', startDragging);
 canvas.addEventListener('mousemove', drag);
 canvas.addEventListener('mouseup', stopDragging);
 canvas.addEventListener('mouseout', stopDragging);
+canvas.addEventListener('dblclick', handleDoubleClick);
 window.addEventListener('resize', resizeCanvas);
 resizeCanvas();
