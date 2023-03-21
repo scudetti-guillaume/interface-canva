128460cb50ceace924983536df3dbfbe5613f909
scudetti-guillaume <124565246+scudetti-guillaume@users.noreply.github.com>
2023-03-21 10:39:43 +0100

    ADD fix bug

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import { Rectangle } from './rectangles.js';
+import { Rectangle } from "./rectangles.js";
 
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-const btnRepaint = document.getElementById('button-color-pair')
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const btnRepaint = document.getElementById("button-color-pair");
 const rectangles = [];
-const excludedColors = []
+const excludedColors = [];
 const notCompleteRotation = [];
-let sortRectangles = [];
 let isDragging = false;
 let doubleClickCheck = false;
 
@@ -15,21 +14,26 @@ const resizeCanvas = () => {
   canvas.width = canvas.parentElement.clientWidth;
   canvas.height = canvas.parentElement.clientHeight;
   redrawRectangles();
-}
+};
 
-// Redessine les rectangles 
+// Redessine les rectangles
 
 const redrawRectangles = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
   rectangles.forEach((rectangle, index) => {
-    if (rectangle.x < 0 || rectangle.y < 0 || rectangle.x + rectangle.width > canvas.width || rectangle.y + rectangle.height > canvas.height ) {
+    if (
+      rectangle.x < 0 ||
+      rectangle.y < 0 ||
+      rectangle.x + rectangle.width > canvas.width ||
+      rectangle.y + rectangle.height > canvas.height ||
+      (notCompleteRotation.length === 0 && rectangle.isSelected === true)
+    ) {
       rectangles.splice(index, 1);
     } else {
       rectangle.draw(context);
     }
-  })
+  });
 };
-
 
 // Donne une couleur aléatoire au rectangle en excluant celle deja existante ou celle du backgroung du canva
 
@@ -48,20 +52,20 @@ const getRandomColor = () => {
     excludedColors.push(color);
     return color;
   }
-}
+};
 
 // Démarre le dessin du rectangle
 
 const startDragging = (event) => {
   event.preventDefault();
   const color = getRandomColor();
-  const colorPair = '';
+  // const colorPair = getRandomColor();
   const startX = event.offsetX;
   const startY = event.offsetY;
-  const rectangle = new Rectangle(startX, startY, 0, 0, color, colorPair, context);
+  const rectangle = new Rectangle(startX, startY, 0, 0, color,context);
   rectangles.push(rectangle);
   isDragging = true;
-}
+};
 
 // Assure le dessin du rectangle
 const drag = (event) => {
@@ -71,50 +75,54 @@ const drag = (event) => {
     rectangle.height = event.offsetY - rectangle.y;
     redrawRectangles();
   }
-}
+};
+
 // Stop le dessin du rectangle
 const stopDragging = () => {
   isDragging = false;
-}
+};
 
 // Animation des rectangles au double click
 
 const animationRectangle = (event) => {
   event.preventDefault();
-  doubleClickCheck = true
+  doubleClickCheck = true;
   const doubleClickX = event.offsetX;
   const doubleClickY = event.offsetY;
   let foundRectangle = null;
   // Supprime du tableau les deux derniers rectangles créés dû au double click et redessine le tableau
-  if (doubleClickCheck && rectangles.length >= 2) { 
+  if (doubleClickCheck && rectangles.length >= 2) {
     rectangles.splice(rectangles.length - 2, 2);
     rectangles.forEach((rectangle) => {
-      if (doubleClickX >= rectangle.x && doubleClickX <= rectangle.x + rectangle.width
-        && doubleClickY >= rectangle.y && doubleClickY <= rectangle.y + rectangle.height) {
+      if (
+        doubleClickX >= rectangle.x &&
+        doubleClickX <= rectangle.x + rectangle.width &&
+        doubleClickY >= rectangle.y &&
+        doubleClickY <= rectangle.y + rectangle.height
+      ) {
         foundRectangle = rectangle;
         foundRectangle.isSelected = true;
-      //   console.log(foundRectangle);
       }
     });
-   // Si le double click est sur un rectangle
+    // Si le double click est sur un rectangle
     if (foundRectangle) {
+      // Tableau des rectangles en cour de rotation
       notCompleteRotation.push(foundRectangle);
-  
-      foundRectangle.animateRotation()
-
-  // L'animation du rectangle
+      // L'animation du rectangle
+      foundRectangle.animateRotation(redrawRectangles);
       const animate = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
         if (foundRectangle.rotation <= foundRectangle.animationRotation) {
           requestAnimationFrame(animate);
-          // foundRectangle.animateRotation()
-          
-  // Condition pour que toutes soit terminée avant suppression des rectangles
         } else {
-          notCompleteRotation.shift()
-       
+          // Condition pour que toutes les animations soit terminée avant suppression des rectangles
+          notCompleteRotation.shift(); // Supression du tableau notCompleteRotation des rectangles ayant finis leur animation
           for (let i = rectangles.length - 1; i >= 0; i--) {
-            if (notCompleteRotation.length === 0 && rectangles[i].isSelected == true) {
+            // Supression du tableau rectangles des rectangles ayant finis leur animation
+            if (
+              notCompleteRotation.length === 0 &&
+              rectangles[i].isSelected == true
+            ) {
+              // Condition de vérification avant supression des rectangles
               rectangles.splice(i, 1);
             }
           }
@@ -124,55 +132,122 @@ const animationRectangle = (event) => {
       requestAnimationFrame(animate);
     }
   }
-}
-
+};
 
 // Repeindre de la même couleur aléatoire les deux rectangles qui ont la plus petite différence d'aire
 
 const repaint = () => {
-let minDiff = Number.MAX_VALUE;
- sortRectangles = [];
+  let minDiff = Number.MAX_VALUE;
+  let sortRectangles = [];
 
+  // Trie les rectangles par ordre croissant d'aire
+  rectangles.sort((a, b) => a.getArea() - b.getArea());
 
- // Trie les rectangles par ordre croissant d'aire
- rectangles.sort((a, b) => a.getArea() - b.getArea());
-  
   // Sélectionne la paire de rectangles ayant la plus petite différence d'aire
-  
+
   for (let i = 0; i < rectangles.length - 1; i++) {
     const diff = rectangles[i + 1].getArea() - rectangles[i].getArea();
     if (diff < minDiff) {
       minDiff = diff;
-     sortRectangles = [rectangles[i], rectangles[i + 1]];  
+      sortRectangles = [rectangles[i], rectangles[i + 1]];
     }
-  // Repeint les deux rectangles sélectionnés avec la même couleur aléatoire
-  let rectangleColorPair = getRandomColor();
-  sortRectangles.forEach(pairRectangle => { 
-    pairRectangle.color = rectangleColorPair
-  pairRectangle.isPaired = true;
-  });
-  
-  // Réinitialise la propriété isPaired pour les rectangles qui ne font pas partie de la paire
-    // rectangles.forEach(rectangle => {
-    //   if (!sortRectangles.includes(rectangle)) {
-    //     rectangle.isPaired = false;
-    //   }
-    // });
-  
   }
+  // Repeint les deux rectangles sélectionnés avec la même couleur aléatoire
+  let colorPair = getRandomColor();
+  sortRectangles.forEach((pairRectangle) => {
+    pairRectangle.isPaired = true;
+    pairRectangle.color = colorPair;
+   
+  });
+
+
+  // rectangles.forEach((rectangle) => {
+  //   if (!sortRectangles.includes(rectangle)) {
+  //     rectangle.isPaired = false;
+  //   }
+  // });
+
+
+
+    // redrawRectangles();
+
+  // } => Pour trier tous les rectangles par paire déplacer la fermeture } de la boucle for précedente ici
+
+  // Réinitialise la propriété isPaired pour les rectangles qui ne font pas partie de la paire
+  // rectangles.forEach(rectangle => {
+  //   if (!sortRectangles.includes(rectangle)) {
+  //     rectangle.isPaired = false;
+  //   }
+  // });
+
+  // sortRectangles = [];
   console.log(rectangles);
-  redrawRectangles()
-}
-
-
-window.addEventListener('resize', resizeCanvas);
-canvas.addEventListener('mousedown', startDragging);
-canvas.addEventListener('mousemove', drag);
-canvas.addEventListener('mouseup', stopDragging);
-canvas.addEventListener('mouseout', stopDragging);
-canvas.addEventListener('dblclick', animationRectangle);
-btnRepaint.addEventListener('click', repaint)
+  redrawRectangles();
+};
+window.addEventListener("resize", resizeCanvas);
+canvas.addEventListener("mousedown", startDragging);
+canvas.addEventListener("mousemove", drag);
+canvas.addEventListener("mouseup", stopDragging);
+canvas.addEventListener("mouseout", stopDragging);
+canvas.addEventListener("dblclick", animationRectangle);
+btnRepaint.addEventListener("click", repaint);
 resizeCanvas();
-repaint()
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const animationRectangle = (event) => {
+//   event.preventDefault();
+//   doubleClickCheck = true
+//   const doubleClickX = event.offsetX;
+//   const doubleClickY = event.offsetY;
+//   // let foundRectangle = null;
+//   // supprime du tableau les deux derniers rectangles créés dû au double click et redessine le tableau
+//   if (doubleClickCheck && rectangles.length >= 2) { // il faut vérifier que le tableau contient au moins deux éléments
+//     rectangles.splice(rectangles.length - 2, 2);
+//     rectangles.forEach((rectangle) => {
+//       if (doubleClickX >= rectangle.x && doubleClickX <= rectangle.x + rectangle.width
+//         && doubleClickY >= rectangle.y && doubleClickY <= rectangle.y + rectangle.height) {
+//         // foundRectangle = rectangle;
+//           rectangle.isSelected = true;
+//           // rectangle.animationInProgress = true;
+//         notCompleteRotation.push(rectangle)
+//         rectangle.animateRotation(context, redrawRectangles, rectangles);
+//         // animate()
+//         // console.log(notCompleteRotation);
+//         console.log(rectangle.animationInProgress);
+//           if (rectangle.animationInProgress === false && rectangle.isSelected === true) {
+
+//             // ne supprimez pas le rectangle s'il y a une animation en cours
+//             notCompleteRotation.shift()
+//             console.log(notCompleteRotation);
+//           }
+//         for (let i = rectangles.length - 1; i >= 0; i--) {
+//             if (notCompleteRotation.length === 0  && rectangles[i].isSelected === true) {
+//               console.log('la');
+//               rectangles.splice(i, 1);
+//             }
+//           }
+//         redrawRectangles();
+//       }
+//     });
+
+//   }}
--- rectangles.js
+++ rectangles.js
@@ -1,20 +1,22 @@
 export class Rectangle {
-    constructor(x, y, width, height, color, colorPair, context) {
+    constructor(x, y, width, height, color,  context) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.area = width * height;
-        // this.color = this.isPaired ? this.colorPair : this.colorNotPair;
-        this.color = color;
+        this.color = color ;
+        // this.originalColor = t
         // this.colorPair = colorPair;
+        this.animationRotation = 360; // Rotation total de l'animation en degré
+        this.animationDuration = 2000 // Durée de l'animation
+        this.animationProgress = 0
         this.rotation = 0;
-        this.animationRotation = 360; 
         this.context = context;
         this.isSelected = false;
         this.isPaired = false
-     
     }
+    
     
     getArea() {
         this.area = this.width * this.height;
@@ -23,35 +25,105 @@ export class Rectangle {
 
     draw() {
         const { context } = this;
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.rect(this.x, this.y, this.width, this.height);
-        // context.fill();
         context.save();
+        context.fillStyle = this.isPaired  ? this.colorPair : this.color;
+        context.fillStyle = this.color;
         context.translate(this.x + this.width / 2, this.y + this.height / 2);
         context.rotate((this.rotation * Math.PI) / 180);
-        // context.fillStyle = this.color;
-        context.rect(-this.width / 2, -this.height / 2, this.width, this.height);
-       context.fill(); // 
-        // context.closePath();
-        
-       context.restore(); 
+        context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height)
+        context.restore(); 
     }
 
-    animateRotation() {
+   
+    animateRotation(redrawRectangles) {
         const { context } = this;
         const start = performance.now();
-        const duration = 1000;
         const animate = (timestamp) => {
-         
-            const progress = timestamp - start;
-            this.rotation = (progress / duration) * this.animationRotation;
-            if (progress < duration) {
-               context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
-                this.draw(); // correction: appeler draw() sans argument
+            this.animationProgress = timestamp - start;
+            this.rotation = (this.animationProgress / this.animationDuration) * this.animationRotation;
+            if (this.animationProgress < this.animationDuration) {
+              context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+                this.draw(); 
                 requestAnimationFrame(animate);
             }
         };
         requestAnimationFrame(animate);
     }
+    
+  
 }
+
+
+  // animateRotation(context, redrawRectangles, rectangles) {
+    //     this.animationInProgress = true;
+    //     const start = performance.now();
+    //     // let animationCount = 0; // étape 1
+    //     // this.animationEndDegree = this.animationRotationTotal; // Changez cette valeur pour modifier l'angle de rotation
+    //     const animate = (timestamp) => {
+    //         const elapsed = timestamp - start;
+    //         this.animationProgress = elapsed / this.animationDuration;
+    //         // this.animationProgress = (elapsed / this.animationDuration) * this.animationRotationTotal;
+    //         if (this.animationProgress < 1) {
+    //             this.rotationDegree = this.animationProgress * this.animationRotation;
+    //             context.clearRect(this.x - this.width / 2, this.y - this.height / 2, this.width * 2, this.height * 2);
+    //             this.draw(context);
+    //             redrawRectangles();
+    //             requestAnimationFrame(animate);
+    //             // console.log(this);
+    //         }
+    //         else {
+    //         console.log('la');
+    //             this.rotationDegree = this.animationRotationTotal;
+    //           return  this.animationInProgress = false;
+              
+              
+    //             // animationCount--; // étape 3
+    //             // if (animationCount === 0) { // étape 4
+    //             //     this.animationInProgress = false;
+    //             //     const index = rectangles.indexOf(this);
+    //             //     if (index > -1) {
+    //             //         rectangles.splice(index, 1);
+    //             //         redrawRectangles();
+    //             //     }
+    //             // }
+    //         }
+    //     };
+    //     // animationCount++; // étape 2
+    //     requestAnimationFrame(animate);
+    // }
+    
+    // removeRectangle(rectangle, rectangles, redrawRectangles){
+    //     if (rectangle.animationInProgress) {
+    //         // ne supprimez pas le rectangle s'il y a une animation en cours
+    //         return;
+    //     }
+    //     const index = rectangles.indexOf(rectangle);
+    //     if (index > -1) {
+    //         rectangles.splice(index, 1);
+    //         redrawRectangles();
+    //     }
+    // }
+
+
+
+
+// draw() {
+//     const { context } = this;
+//     context.save();
+//     context.fillStyle = this.color;
+//     // context.beginPath();
+//     // context.rect(this.x, this.y, this.width, this.height);
+//     // context.fill();
+
+//     context.translate(this.x + this.width / 2, this.y + this.height / 2);
+//     context.rotate((this.rotation * Math.PI) / 180);
+//     // context.fillStyle = this.color;
+//     context.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+//     context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height)
+//     //    context.fill(); // 
+//     // context.closePath();
+
+//     context.restore();
+// }
+
+
